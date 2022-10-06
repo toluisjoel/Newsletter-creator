1b5685f51928a35de0510fa40a0e6228f1e8fad2
Tolu Joel <toluisjoel@gmail.com>
2022-10-06 14:54:41 +0100

    viewsets and serializers

--- news/views.py
+++ news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.filter(status='draft', validated=True)
+    serializer_class = PostSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
