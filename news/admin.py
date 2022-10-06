6c5f83d327d40028b52959e5b0898dbb908a864c
Tolu Joel <toluisjoel@gmail.com>
2022-10-06 13:04:03 +0100

    post model added

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'source', 'created_at', 'status', 'validated']
+    list_filter = ['validated', 'status', 'created_at']
+    list_per_page = 10
+
+admin.site.register(Post, PostAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'draft'),
+        ('published', 'published'),
+    )
+    
+    source = models.URLField(blank=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.URLField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    validated = models.BooleanField(default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('created_at',)
+    
+    def __str__(self):
+        return self.title
