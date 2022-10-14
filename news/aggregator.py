bb25c1ef005e459ababf175dbde06a6f202d24a7
Tolu Joel <toluisjoel@gmail.com>
2022-10-14 18:57:38 +0100

    celery aggregating news

--- news/aggregator.py
+++ news/aggregator.py
@@ -1,22 +1,38 @@
+# scraping
 import json
+from datetime import datetime
+
+import lxml
 import requests
 from bs4 import BeautifulSoup
+from celery import shared_task
 
-from .models import Post
+from news.models import Post
 
 
+# scraping function
+@shared_task
 def aggregate(url, tag, tag_class):
     site = requests.get(url)
     soup = BeautifulSoup(site.text, 'lxml')
     posts = soup.find_all(tag, class_=tag_class)
-    
+    article_list = []
+
     for news in posts:
         source = news.a['href']
         title = news.text
         image = news.find('img')
-        
-        news = Post.objects.create(source=source, title=title, content=title, image=image)
-        news.save()
+
+        article = {
+            'title': title,
+            'link': source,
+        }
+        article_list.append(article)
+        return save_function(article_list)
+
+        # news = Post.objects.create(
+        #     source=source, title=title, content=title, image=image)
+        # news.save()
 
 
 if __name__ == "__main__":
@@ -25,3 +41,22 @@ if __name__ == "__main__":
 
         for _link, _tag, _class in sites.values():
             aggregate(_link, _tag, _class)
+
+
+@shared_task(serializer='json')
+def save_function(article_list):
+    print('starting')
+    new_count = 0
+
+    for article in article_list:
+        try:
+            Post.objects.create(
+                title=article['title'],
+                link=article['link'],
+                published=article['published'],
+                source=article['source']
+            )
+            new_count += 1
+        except:
+            pass
+    return print('finished')
