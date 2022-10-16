3d158d51d6f1e91bad597951de00d005667911aa
Tolu Joel <toluisjoel@gmail.com>
2022-10-16 22:17:23 +0100

    Added 'delivered' and 'opened' email fields

--- subscribers/models.py
+++ subscribers/models.py
@@ -8,9 +8,11 @@ class Subscriber(models.Model):
 
     otp_num = models.CharField(max_length=50)
     confirmed = models.BooleanField(default=False)  # True if subscriber confirms email
+    
+    delivered_emails = models.IntegerField(default=0)
+    opened_emails = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.email
@@ -20,8 +22,11 @@ class Unsubscriber(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(blank=True, max_length=25)
     email = models.EmailField(unique=True)
+    
+    delivered_emails = models.IntegerField(default=0)
+    opened_emails = models.IntegerField(default=0)
+    
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.email
--- subscribers/signals.py
+++ subscribers/signals.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
@@ -11,33 +11,11 @@ from .models import Subscriber, Unsubscriber
 from .tokens import account_activation_token
 
 
-@receiver(post_save, sender=Subscriber)
-def my_callback(sender, instance, created, **kwargs):    
-    if created:
-        # Delete user email from unsubscribers list, if they have once unsubscribed
-        try:
-            unsubscriber = Unsubscriber.objects.get(email=instance.email)
-            unsubscriber.delete()
-        except Unsubscriber.DoesNotExist:
-            pass
+@receiver(pre_save, sender=Subscriber)
+def my_callback(sender, instance, **kwargs):
+    try:
+        unsubscriber = Unsubscriber.objects.get(email=instance.email)
+        unsubscriber.delete()
+    except Unsubscriber.DoesNotExist:
+        pass
 
-        # Send Confirmation Email
-        email_context = {
-            'subscriber': instance,
-            'action': 'added',
-            'domain': '127.0.0.1:8000',  # get_current_site(request).domain
-            'uid': urlsafe_base64_encode(force_bytes(instance.pk)),
-            'token': account_activation_token.make_token(instance),
-            'protocol': 'http',  # 'https' if request.is_secure() else 'http'
-        }
-        html_content = render_to_string('emails/confirm_subscription.html', email_context)
-        plain_text_content = strip_tags(html_content)
-
-        send_mail(
-            'Subscription Confirmation',
-            plain_text_content,
-            settings.DEFAULT_FROM_EMAIL,
-            [instance.email],
-            html_message=html_content,
-            fail_silently=False,
-        )
