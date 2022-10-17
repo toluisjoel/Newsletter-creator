da7e2506c29a6f5d19aa91f6b9f14fe928903713
Tolu Joel <toluisjoel@gmail.com>
2022-10-17 11:08:31 +0100

    removed unused imports

--- subscribers/signals.py
+++ subscribers/signals.py
@@ -1,14 +1,7 @@
-from django.conf import settings
-from django.core.mail import send_mail
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.html import strip_tags
-from django.utils.http import urlsafe_base64_encode
 
 from .models import Subscriber, Unsubscriber
-from .tokens import account_activation_token
 
 
 @receiver(pre_save, sender=Subscriber)
@@ -18,4 +11,3 @@ def my_callback(sender, instance, **kwargs):
         unsubscriber.delete()
     except Unsubscriber.DoesNotExist:
         pass
-
