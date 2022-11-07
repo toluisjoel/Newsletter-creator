2fc75f236ae328be6106b3b15c1d8af2443d3626
Tolu Joel <toluisjoel@gmail.com>
2022-11-07 02:54:38 +0100

    urls.py refactoring

--- news/urls.py
+++ news/urls.py
@@ -6,10 +6,15 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.newsboard, name='newsboard'),
+    
+    # posts
     path('post/', views.PostList.as_view(), name='post_list'),
     path('post/<str:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('create/post/', views.CreatePost.as_view(), name='create_post'),
+    
+    # letters
     path('letter/', views.NewsLetterList.as_view(), name='letter_list'),
     path('create/letter/', views.CreateLetter.as_view(), name='create_letter'),
     path('letter/<str:pk>/', views.NewsLetterDetail.as_view(), name='letter_detail'),
+    path('previous-news/', views.PreviousNewsLetterList.as_view(), name='previous_news_list'),
 ]
